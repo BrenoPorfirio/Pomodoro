67af2d1327bd8c37c30e6757c328aad87abe5d5a
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-26 00:23:14 -0300

    import styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,11 @@
+import styles from './Button.module.css'
+
 interface ButtonProps {
     color?: 'primary' | 'secondary' | 'danger' | 'success';
 }
 
-export function Button(props: ButtonProps) {
+export function Button({ color= 'primary' }: ButtonProps) {
     return (
-        <button>ENVIAR</button>
+        <button className={`${styles.button} ${styles[color]}`}>ENVIAR</button>
     )
 }
