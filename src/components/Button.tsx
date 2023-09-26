3154b270eab9a5ba64e27f96fc92366468e2619a
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-26 00:15:21 -0300

    interface in buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,8 @@
-export function Button() {
+interface ButtonProps {
+    color?: 'primary' | 'secondary' | 'danger' | 'success';
+}
+
+export function Button(props: ButtonProps) {
     return (
         <button>ENVIAR</button>
     )
