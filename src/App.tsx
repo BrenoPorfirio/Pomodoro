16de61c0297ccc6704867c9f4b21928ca403b3d9
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-26 00:16:40 -0300

    coloring in buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Button } from './components/Button';
 export function App() {
   return (
     <>
+      <Button color="primary" />
+      <Button color="secondary" />
+      <Button color="success" />
+      <Button color="danger" />
       <Button />
     </>
   )
