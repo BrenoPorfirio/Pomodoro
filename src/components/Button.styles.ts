f1645470274d403d12a864d35054949fee7ff827
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-27 19:43:21 -0300

    utilizing theme props

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
+
+    /* ${props => {
         return css`
         background-color: ${buttonVariants[props.variant]}
         `   
-    }}
+    }} */
 `
