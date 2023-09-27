8f8cd8f66d597114a908389676e1061c357f9b74
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-27 19:56:24 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
@@ -10,6 +12,8 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
