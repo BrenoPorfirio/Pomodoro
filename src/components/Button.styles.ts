e25cf39b55a7012bbbe88fd92220a81f194a9076
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-27 20:07:31 -0300

    utilizing initial colors

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme['green-500']};
     color: ${props => props.theme.white};
 
     /* ${props => {
