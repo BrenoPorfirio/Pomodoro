80544feaae657ebb960708430667047fe4563305
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-28 21:57:01 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
-
+import { BrowserRouter } from 'react-router-dom';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter >
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
