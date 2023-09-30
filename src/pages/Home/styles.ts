f1999cd77a2a573ff4cdd44945fb766f3ac39858
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-30 18:07:19 -0300

    add Separator

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -43,3 +43,13 @@ export const CounterdownContainer = styled.main `
         border-radius: 8px;
     }
 `;
+
+export const Separator = styled.div `
+    padding: 2rem 0;
+    color: ${(props) => props.theme['green-500']};
+
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+`
