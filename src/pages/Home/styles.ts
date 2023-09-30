31dcbf6e80ef7499868fd803fdb2f2d0fd82e4c3
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-30 17:57:14 -0300

    style for HomeContainer

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main `
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3.5rem;
+        
+    }
+`;
+
+export const FormContainer = styled.main ``;
+
+export const CounterdownContainer = styled.main ``;
