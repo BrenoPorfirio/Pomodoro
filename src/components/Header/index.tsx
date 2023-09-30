fc10858400b06ac1efff89318fed0724efb298b1
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-29 21:48:05 -0300

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import { HeaderContainer } from "./styles";
+
 export function Header () {
-    return <h1>Header</h1>
+    return (
+        <HeaderContainer>
+            <span>Logo</span>
+            <nav>
+                <a href="">
+                    Timer
+                </a>
+                <a href="">
+                    History
+                </a>
+            </nav>
+        </HeaderContainer>
+    )
 }
