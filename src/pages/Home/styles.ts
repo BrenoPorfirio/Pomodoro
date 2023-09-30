34bc890310e5ba28ba13814b3033706e88479996
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-30 18:02:21 -0300

    style for FormContainer

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -12,10 +12,21 @@ export const HomeContainer = styled.main `
         flex-direction: column;
         align-items: center;
         gap: 3.5rem;
-        
+
     }
 `;
 
-export const FormContainer = styled.main ``;
+export const FormContainer = styled.main `
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    color: ${(props) => props.theme['gray-100']};
+    font-size: 1.125rem;
+    font-weight: bold;
+    flex-wrap: wrap;
+`;
 
 export const CounterdownContainer = styled.main ``;
