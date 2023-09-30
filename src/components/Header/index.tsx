0428f9b474717cf42dc4c851c5d5ade01037f7a2
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-29 21:57:57 -0300

    put logo and icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from './styles';
+
+import LogoPomodoro from '../../assets/LogoPomodoro.svg'
+import { Timer, Scroll } from 'phosphor-react';
 
 export function Header () {
     return (
         <HeaderContainer>
+            <img src={LogoPomodoro} />
             <span>Logo</span>
             <nav>
                 <a href="">
-                    Timer
+                    <Timer size={24}/>
                 </a>
                 <a href="">
-                    History
+                    <Scroll size={24}/>
                 </a>
             </nav>
         </HeaderContainer>
