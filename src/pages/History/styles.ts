5fefa4cc6e71b766700a3bd76f246559863f4377
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-02 22:12:24 -0300

    style HistoryContainer

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -2,7 +2,15 @@ import styled from "styled-components";
 
 
 export const HistoryContainer = styled.main `
+    flex: 1;
+    padding: 3.5rem;
+    display: flex;
+    flex-direction: column;
 
+    h1 {
+        font-size: 1.5rem;
+        color: ${(props) => props.theme['gray-100']};
+    }
 `
 
 
