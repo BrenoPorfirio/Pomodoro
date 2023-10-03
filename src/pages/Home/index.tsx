8a6eb4665cec0a77f1291281938e6f750e92ef37
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-02 21:56:07 -0300

    min and max time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,6 +19,9 @@ export function Home() {
                 type="number"
                 id="minutesAmount"
                 placeholder="00"
+                step={5}
+                min={5}
+                max={60}
                 />
 
                 <span>minutos.</span>
