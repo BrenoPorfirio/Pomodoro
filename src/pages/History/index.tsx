5c8c0a24ce29bc1d77597f78f3345e2bd81aa9ea
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-02 22:04:30 -0300

    create HistoryContainer and HistoryList

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,13 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+    return (
+    <HistoryContainer>
+        <h1>Meu histórico</h1>
+
+        <HistoryList>
+            
+        </HistoryList>
+    </HistoryContainer>
+    )
+}
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const HistoryContainer = styled.main `
+
+`
+
+
+export const HistoryList = styled.main `
+    
+`
