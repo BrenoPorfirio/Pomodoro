d1cbe5da035ad48887db82dfde3e2bfa5736369b
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-02 22:25:45 -0300

    style HistoryList

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -13,7 +13,50 @@ export const HistoryContainer = styled.main `
     }
 `
 
-
 export const HistoryList = styled.main `
+    flex: 1;
+    overflow: auto;
+    margin-top: 2rem;
     
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        min-width: 600px;
+        
+        th {
+            background-color: ${(props) => props.theme['gray-600']};
+            padding: 1rem;
+            text-align: left;
+            color: ${(props) => props.theme['gray-100']};
+            font-size: 0.875rem;
+            line-height: 1.6;
+
+            &:first-child {
+                border-bottom-left-radius: 8px;
+                padding-left: 1.5rem;
+            }
+
+            &:last-child {
+                border-top-right-radius: 8px;
+                padding-right: 1.5rem;
+            }
+        }
+
+        td {
+            background-color: ${(props) => props.theme['gray-700']};
+            border-top: 4px solid ${(props) => props.theme['gray-800']};
+            padding: 1rem;
+            font-size: 0.875rem;
+            line-height: 1.6;
+
+            &:first-child {
+                width: 50%;
+                padding-left: 1.5rem;
+            }
+
+            &:last-child {
+                padding-right: 1.5rem;
+            }
+        }
+    }
 `
