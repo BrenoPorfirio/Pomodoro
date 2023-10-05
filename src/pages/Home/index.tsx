b62736d96f29081bffb41d9334e87c320ab05685
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-04 23:13:11 -0300

    using zod validations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,13 +13,23 @@ import {
     MinutesAmountInput
     } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number()
+    .min(5, 'O ciclo precisa de no minimo 5 minutos')
+    .max(60, 'O ciclo precisa de no máximo 60 minutos'),
+})
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
 
-    function handleCreateNewCycle(data: unknown) {
+    function handleCreateNewCycle(data: any) {
         console.log(data)
     }
+
+    console.log(formState.errors)
 
     const task = watch('task')
     const isSubmitDisabled = !task
@@ -50,7 +60,7 @@ export function Home() {
                 placeholder="00"
                 step={5}
                 min={5}
-                max={60}
+                // max={60}
                 {...register('minutesAmount', { valueAsNumber: true})}
                 />
 
