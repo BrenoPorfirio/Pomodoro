5ad704fbe624ef3c8a4b77026b52273a286f46c8
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-04 22:55:44 -0300

    submit disable using watch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,15 +13,25 @@ import {
 
 
 export function Home() {
+    const { register, handleSubmit, watch } = useForm()
+
+    function handleCreateNewCycle(data: unknown) {
+        console.log(data)
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
     return(
     <HomeContainer>
-        <form action="">
+        <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
             <FormContainer>
-                <label htmlFor="">Vou trabalhar em</label>
+                <label htmlFor="task">Vou trabalhar em</label>
                 <TaskInput 
                 id="task"
                 list="task-suggestions"
                 placeholder="Dê um nome para seu projeto"
+                {...register('task')}
                 />
 
                 <datalist id="task-suggestions">
@@ -39,6 +49,7 @@ export function Home() {
                 step={5}
                 min={5}
                 max={60}
+                {...register('minutesAmount', { valueAsNumber: true})}
                 />
 
                 <span>minutos.</span>
@@ -51,7 +62,7 @@ export function Home() {
                 <span>0</span>
                 <span>0</span>
             </CounterdownContainer>
-            <StartCounterdownButton disabled type="submit">
+            <StartCounterdownButton  disabled={isSubmitDisabled} type="submit">
                 <Play size={24}/>
                 Começar
             </StartCounterdownButton>
