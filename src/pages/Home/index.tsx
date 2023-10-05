5bccbf7dafb86881e01dcf7234d2487f1ee08c2e
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-04 23:26:48 -0300

    add reset

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,7 +28,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData  = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -38,6 +38,7 @@ export function Home() {
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data)
+        reset();
     }
 
     const task = watch('task')
