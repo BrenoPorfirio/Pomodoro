7d3ab98b032f79fbc756e2c8f143214183fba2b0
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-08 20:04:40 -0300

    modularization NewCycleForm

---
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -0,0 +1,37 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles"
+
+export function NewCycleForm() {
+    return (
+        <FormContainer>
+                <label htmlFor="task">Vou trabalhar em</label>
+                <TaskInput 
+                id="task"
+                list="task-suggestions"
+                placeholder="Dê um nome para seu projeto"
+                disabled={!!activeCycle}
+                {...register('task')}
+                />
+
+                <datalist id="task-suggestions">
+                    <option value="Projeto 1"></option>
+                    <option value="Beber água"></option>
+                    <option value="Programar"></option>
+                    <option value="Descansar"></option>
+                </datalist>
+
+                <label htmlFor="">durante</label>
+                <MinutesAmountInput
+                type="number"
+                id="minutesAmount"
+                placeholder="00"
+                step={5}
+                min={1}
+                max={60}
+                disabled={!!activeCycle}
+                {...register('minutesAmount', { valueAsNumber: true})}
+                />
+
+                <span>minutos.</span>
+            </FormContainer>
+    )
+}
