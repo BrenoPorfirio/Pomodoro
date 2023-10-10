2f89524918b59b8853efdb3b6867d141228692dc
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-09 21:58:45 -0300

    config eslint

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,52 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const HomeContainer = styled.main `
-    flex: 1;
+export const HomeContainer = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    gap: 3.5rem;
+  }
+`
 
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 3.5rem;
-    }
-`;
+export const BaseCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  color: ${(props) => props.theme['gray-100']};
 
-export const BaseCountdownButton = styled.button `
-    width: 100%;
-    border: 0;
-    padding: 1rem;
-    border-radius: 8px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-    font-weight: bold;
-    cursor: pointer;
-    color: ${(props => props.theme['gray-100'])};
-
-    &:disabled {
-        opacity: 0.7;
-        cursor: not-allowed;
-    }
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
 `
 
 export const StartCountdownButton = styled(BaseCountdownButton)`
-    background: ${(props => props.theme['green-500'])};
+  background: ${(props) => props.theme['green-500']};
 
-    &:not(:disabled):hover {
-        background: ${(props => props.theme['green-700'])};        
-    }
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
 
 export const StopCountdownButton = styled(BaseCountdownButton)`
-    background: ${(props => props.theme['red-500'])};
+  background: ${(props) => props.theme['red-500']};
 
-    &:not(:disabled):hover {
-        background: ${(props => props.theme['red-700'])};        
-    }
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
+  }
 `
-
