db0f317c469daeab0cff162a8522778b90f8e580
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-09 22:00:00 -0300

    eslint config

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,54 +1,55 @@
-import { useEffect, useState } from "react";
-import { CounterdownContainer, Separator } from "./styles";
-import { differenceInMilliseconds } from "date-fns";
+import { useEffect, useState } from 'react'
+import { CounterdownContainer, Separator } from './styles'
+import { differenceInMilliseconds } from 'date-fns'
 
 interface CountdownProps {
-    activeCycle: any;
+  activeCycle: any
 }
 
-export function Countdown({ activeCycle }: CountdownProps ) {
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+export function Countdown({ activeCycle }: CountdownProps) {
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
-    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
-    useEffect(() => {
-        let interval: number;
+  useEffect(() => {
+    let interval: number
 
-        if (activeCycle) {
-            interval = setInterval(() => {
-                const secondsDifference = differenceInMilliseconds (
-                    new Date(),
-                    activeCycle.startDate,
-                    )
-                    if (secondsDifference >= totalSeconds) {
-                        setCycles((state) => state.map((cycle) => {
-                            if (cycle.id === activeCycleId) {
-                                return {...cycle, finishedDate: new Date()}
-                            } else {
-                                return cycle
-                            }
-                        }),
-                    )
-                    setAmountSecondsPassed(totalSeconds)
-                    clearInterval(interval)
-                } else {
-                    setAmountSecondsPassed(secondsDifference);
-                }
-            }, 1000)
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInMilliseconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+          )
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference)
         }
+      }, 1000)
+    }
 
-        return () => {
-            clearInterval (interval)
-        }
-    }, [activeCycle, totalSeconds, activeCycleId])
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle, totalSeconds, activeCycleId])
 
-    return (
-        <CounterdownContainer>
-            <span>{minutes[0]}</span>
-            <span>{minutes[1]}</span>
-            <Separator>:</Separator>
-            <span>{seconds[0]}</span>
-            <span>{seconds[1]}</span>
-        </CounterdownContainer>
-    )
+  return (
+    <CounterdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CounterdownContainer>
+  )
 }
--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,58 +1,58 @@
-import { useForm } from "react-hook-form"
-import { FormContainer, MinutesAmountInput, TaskInput } from "./styles"
-import * as zod from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, 'Informe a tarefa'),
-    minutesAmount: zod.number()
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
     .min(1, 'O ciclo precisa de no minimo 5 minutos')
     .max(60, 'O ciclo precisa de no máximo 60 minutos'),
 })
 
-
-type NewCycleFormData  = zod.infer<typeof newCycleFormValidationSchema>
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function NewCycleForm() {
-    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema),
-        defaultValues: {
-            task: '',
-            minutesAmount: 0,
-        }
-    })
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
-    return (
-        <FormContainer>
-                <label htmlFor="task">Vou trabalhar em</label>
-                <TaskInput 
-                id="task"
-                list="task-suggestions"
-                placeholder="Dê um nome para seu projeto"
-                disabled={!!activeCycle}
-                {...register('task')}
-                />
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
 
-                <datalist id="task-suggestions">
-                    <option value="Projeto 1"></option>
-                    <option value="Beber água"></option>
-                    <option value="Programar"></option>
-                    <option value="Descansar"></option>
-                </datalist>
+      <datalist id="task-suggestions">
+        <option value="Projeto 1"></option>
+        <option value="Beber água"></option>
+        <option value="Programar"></option>
+        <option value="Descansar"></option>
+      </datalist>
 
-                <label htmlFor="">durante</label>
-                <MinutesAmountInput
-                type="number"
-                id="minutesAmount"
-                placeholder="00"
-                step={5}
-                min={1}
-                max={60}
-                disabled={!!activeCycle}
-                {...register('minutesAmount', { valueAsNumber: true})}
-                />
+      <label htmlFor="">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
 
-                <span>minutos.</span>
-            </FormContainer>
-    )
+      <span>minutos.</span>
+    </FormContainer>
+  )
 }
