8962076f30ee0269b99e2fdc4788d0ba4bb0d7e5
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-12 17:55:59 -0300

    cycles in history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,47 +1,51 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { useContext } from 'react'
+import { HistoryContainer, HistoryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function History() {
-    return (
+  const { cycles } = useContext(CyclesContext)
+  return (
     <HistoryContainer>
-        <h1>Meu histórico</h1>
-        <HistoryList>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tarefa</th>
-                        <th>Duração</th>
-                        <th>Início</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Tarefa</td>
-                        <td>20 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Tarefa</td>
-                        <td>20 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="yellow">Em andamento</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Tarefa</td>
-                        <td>20 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="red">Interrompido</Status>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </HistoryList>
+      <h1>Meu histórico</h1>
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </HistoryContainer>
-    )
+  )
 }
