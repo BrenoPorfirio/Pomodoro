bf1b3e6f237a255023a5db1740b8f37fcf472c94
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-12 17:10:03 -0300

    eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
-import { Router } from './Router';
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter >
+      <BrowserRouter>
         <Router />
       </BrowserRouter>
       <GlobalStyle />
--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,13 @@ import { Home } from './pages/Home'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
-export function Router () {
-    return (
-        <Routes >
-            <Route path='/' element={<DefaultLayout/>}>
-                <Route path='/' element={<Home />} />
-                <Route path='/history' element={<History />}/>
-            </Route>
-        </Routes>
-    )
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
 }
