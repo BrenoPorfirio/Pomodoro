bd0888dbd832e064499529181ff72b8abc1042d0
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-10-12 18:08:36 -0300

    remove reset

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -67,8 +67,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
